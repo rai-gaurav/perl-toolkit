43de689202d3c1eb7f985c275d9beccdb582ca94
grai <gauravrai7860@gmail.com>
2019-09-15 08:20:05 +0000

    Update multi_line_chart.pl

--- Charts/multi_line_chart.pl
+++ Charts/multi_line_chart.pl
@@ -4,5 +4,16 @@ use CreateCharts qw(generate_chart);
 
 
 my $chart_out_file = "chart.png";
+=for
+json data example-
+{
+	key: [],
+	value: {
+		Axis1:[],
+		Axis2:[],
+		Axis3:[]
+	}
+}
+=cut
 my $data_in_json;
 generate_chart($chart_out_file, $data_in_json);
