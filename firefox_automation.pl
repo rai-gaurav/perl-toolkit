f2af6ad85fd35d502a6fa23c4b985c08f93acffc
grai <gauravrai7860@gmail.com>
2019-09-20 09:10:09 +0000

    Update firefox_automation.pl

--- firefox_automation.pl
+++ firefox_automation.pl
@@ -1,1 +1,20 @@
+use strict;
+use warnings;
+use Firefox::Marionette();
+ 
+my $firefox = Firefox::Marionette->new( visible => 1 );
+my $window_handle = $firefox->new_window(type => 'tab', focus => 1);
+$firefox->switch_to_window($window_handle);
+$firefox->go('https://metacpan.org/');
+ 
+#say $firefox->html();
+ 
+# $firefox->find_class('container-fluid')->find_id('search-input')->type('Test::More');
+ 
+# my $file_handle = $firefox->selfie(highlights => [ $firefox->find_name('lucky') ]);
+ 
+# $firefox->find('//button[@name="lucky"]')->click();
+ 
+ #$firefox->await(sub { $firefox->interactive() && $firefox->find_partial('Download') })->click();
 
+ sleep 5;
