7419425ba684d59531361a60c3df392d7953c8fe
grai <gauravrai7860@gmail.com>
2019-09-26 13:48:34 +0000

    Update and rename check_file_integrity.pl to get_checksum.pl

---
+++ get_checksum.pl
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+
+
+# HashAlgorithm choices: MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512
+sub get_checksum {
+    my ( $path_to_file, $hash_algorithm ) = @_;
+    my $checksum;
+    my $os = $^O;
+    eval {
+        if ( -s $path_to_file ) {
+            if ( $os =~ /MSWin32/i ) {
+                $checksum = (split('\n', `C:/Windows/System32/certutil.exe -hashfile $path_to_file $hash_algorithm`))[1];
+            }
+            else {
+                my $cmd = "/usr/bin/" . lc($hash_algorithm) . "sum $path_to_file";
+                $checksum = ( split( ' ', `$cmd` ) )[0];
+            }
+        }
+        else {
+            $checksum = "File is empty";
+        }
+    };
+    if ($@) {
+        print ("Unable to calculate the $hash_algorithm for $path_to_file : $@");
+    }
+    return $checksum;
+}
+
+# Usage
+my $checksum = get_checksum("/home/grai/test.txt", "SHA256");
+print $checksum;
