1580314b857ab6880dfa860256b0dbd5161b0715
sudo-batman <gauravrai7860@gmail.com>
2020-05-11 14:26:41 +0100

    Create start_myapp.sh

---
+++ uwsgi((PSGI)/start_myapp.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script will be runned by docker
+
+# Restrat Web server(here Apache)
+service apache2 restart
+
+# start MyApp
+while getopts "m:" opt; do
+    case ${opt} in
+        m)
+            mode="${OPTARG}"
+            echo "Running in '$mode' mode"
+            if [ "$mode" == "development" ]; then
+                # Since we are using uwsgi, hence it is commented
+                #exec morbo -l "https://*:7070" script/myapp
+                exec uwsgi --ini etc/uwsgi.conf:$mode
+            elif [[ "$mode" = "production" || "$mode" = "staging" ]]; then
+                #exec hypnotoad -f script/myapp
+                exec uwsgi --ini etc/uwsgi.conf:$mode
+            else 
+                echo "Wrong mode provided. Accepted value - development, staging or production" 1>&2
+            fi
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+    esac
+done
